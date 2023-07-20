5b8554a5ba171b843ebf7b2533d406b5b889d3db
sanjheeg <60531063+sanjheeg@users.noreply.github.com>
2023-07-20 13:24:38 -0700

    home and about structures

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template
+import pandas as pd
 
 # class creates website object instances
-app = Flask("Website")
-
+app = Flask(__name__)
 
 # the @ is a decorator, it connects the route method with the function (home in this case)
-@app.route("/home")
+@app.route("/")
 def home():
-    return render_template("tutorial.html")
+    return render_template("home.html")
 
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    return render_template("about.html")
 
 app.run(debug=True)
 
