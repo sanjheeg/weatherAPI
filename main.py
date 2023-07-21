c7124fc32e326a7dcc959b7ce490c9ba6df4bd49
sanjheeg <60531063+sanjheeg@users.noreply.github.com>
2023-07-21 13:47:44 -0700

    station and date finished

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def home():
     return render_template("home.html")
 
 @app.route("/api/v1/<station>/<date>")
-def about(station, date):
+def station(station, date):
     filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
     df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
     date_form = str(date[:4]) + "-" + str(date[4:6]) + "-" + str(date[6:])
