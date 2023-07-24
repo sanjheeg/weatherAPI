acabec9ce779545a3f8159701eda1367f427a77c
sanjheeg <60531063+sanjheeg@users.noreply.github.com>
2023-07-24 09:57:03 -0700

    add url endpoints for yearly and station

--- main.py
+++ main.py
@@ -7,15 +7,16 @@ app = Flask(__name__)
 # the @ is a decorator, it connects the route method with the function (home in this case)
 
 stations = pd.read_csv("data_small/stations.txt", skiprows=17)
-#print(stations)
 stations = stations[["STAID", "STANAME                                 "]]
 
 @app.route("/")
 def home():
     return render_template("home.html", data=stations.to_html())
 
+
+# data for certain station for certain year
 @app.route("/api/v1/<station>/<date>")
-def station(station, date):
+def full_result(station, date):
     filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
     df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
     date_form = str(date[:4]) + "-" + str(date[4:6]) + "-" + str(date[6:])
@@ -23,6 +24,24 @@ def station(station, date):
     return {"station": station,
             "date": date,
             "temperature": temperature}
+
+
+# all the data for a particular station
+@app.route("/api/v1/<station>")
+def station_result(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")
+    return result
+
+
+@app.route("/api/v1/annual/<station>/<year>")
+def year_result(station, year):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20,)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return result
 
 
 if __name__ == "__main__":
